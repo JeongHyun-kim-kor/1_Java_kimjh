5e97087d2b81355ab16587899dffc7b9a9cd6b69
dudgus197493 <70511849+JeongHyun-kim-kor@users.noreply.github.com>
2022-08-25 11:58:07 +0900

    0825 3교시 제네릭, 학생추가

--- 10_collection/src/edu/kh/collection/model/service/StudentService.java
+++ 10_collection/src/edu/kh/collection/model/service/StudentService.java
@@ -1,5 +1,63 @@
 package edu.kh.collection.model.service;
 
-public class StudentService {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+import edu.kh.collection.model.vo.Student;
+
+public class StudentService {
+	
+	// 부모 타입 참조 변수 List 
+	private List<Student> stdList = new ArrayList<Student>();
+			// Student타입으로 제한된 ArrayList 객체 생성
+
+	/** stdList에 학생 정보 추가 메서드
+	 * @param name 
+	 * @param age
+	 * @param address
+	 * @param gender
+	 * @param score
+	 * @return 
+	 * true  : 학생 정보 추가 성공
+	 * false : 학생 정보 추가 실패
+	 */
+	public boolean addStudent(String name, int age, String address, char gender, int score) {
+		
+		
+		
+		
+		
+//		// 제네릭 확인 테스트
+//		List test1 = new ArrayList() ;
+//		
+//		List<String> test2 = new ArrayList<String>() ;
+//		
+//		List<Integer> test3 = new ArrayList<Integer>() ;
+		
+//		List<Student>
+		// List.add(Student e) 이렇게 됨
+		
+		// List.add(E e) : 제네릭<E>에 작성되는 타입으로 모든 E가 변화함
+//		test1.add(Object e); // test1.add(Object e) -> 타입 제한 X , 모든 참조 변수가 Object타입
+//		test2.add(address);; // test2.add(String e) -> 타입 제한 O, 모든 참조 변수가 String타입
+//		test3.add(null); // test3.add(Integer e) -> 타입 제한 O, 모든 참조 변수가 Integer타입
+		
+		Student std = new Student(name, age, address, gender, score);
+		
+		
+		// add(Student e)
+		// boolean add(Student e ) : add() 결과로 boolean 반환
+		return stdList.add(std);
+				// true > 반환(return)
+				
+				
+				
+		
+		
+		
+		
+	}
+	
+	
+	
+ }
--- 10_collection/src/edu/kh/collection/view/StudentView.java
+++ 10_collection/src/edu/kh/collection/view/StudentView.java
@@ -34,6 +34,19 @@ public class StudentView {
 			sc.nextLine();
 			System.out.println();
 			
+			switch(input) {
+			case 1 : addStudent(); break;
+			case 2 : break;
+			case 3 : break;
+			case 4 : break;
+			case 5 : break;
+			case 6 : break;
+			case 0 : System.out.println("프로그램 종료"); break;
+			
+			default : System.out.println("잘못 입력하셨습니다.");
+			}
+			System.out.println();
+			
 			} catch(InputMismatchException e) {
 				System.out.println("[Error] 입력 형식이 유효하지 않습니다. \n");
 				
@@ -48,7 +61,50 @@ public class StudentView {
 		
 	}
 	
-	
+	 /**
+	 * 1. 학생 정보 추가 메서드
+	 */
+	public void addStudent() {
+		 System.out.println("===== 학생 정보 추가 =====");
+		 
+		 // 이름, 나이, 주소, 성별(M/F), 점수
+		 System.out.print("이름 : ");
+		 String name = sc.next();
+		 
+		 System.out.print("나이 : ");
+		 int age= sc.nextInt();
+		 sc.nextLine(); // 입력버퍼 개행문자 제거
+		 
+		 System.out.print("주소 : ");
+		 String address = sc.nextLine();
+		 
+		 char gender;
+		 
+		 while(true) {
+		 System.out.print("성별(M/F) : ");
+		 gender = sc.next().toUpperCase().charAt(0);
+		 //String.toUpperCase() : 문자열을 대문자로 변환
+		 
+		 // 유효성 검사
+		 if(gender == 'M' || gender =='F') {
+			 break; //제대로 입력했을 때만 멈추고 다음으로 넘어가겠다.
+		 } else {
+			 System.out.println("[Error] M / F만 입력해주세요.");
+		 }
+		 }
+		 
+		 System.out.print("점수 : ");
+		 int score = sc.nextInt();
+		 
+		  //제대로 추가 됐으면 true 
+		 if(service.addStudent(name,age,address,gender,score)) {
+			 System.out.println("[추가 완료]");
+		 } else {
+			 System.out.println("[Error] 학생 정보 추가 실패");
+		 }
+		 
+		 
+	 }
 	
 	
 	
