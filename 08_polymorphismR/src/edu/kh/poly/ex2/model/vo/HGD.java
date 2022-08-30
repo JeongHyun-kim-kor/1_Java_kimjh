4ab52da3f1a04617d9631e0b30baff016c9ae092
JeongHyun-kim-kor <70511849+JeongHyun-kim-kor@users.noreply.github.com>
2022-08-31 01:05:35 +0900

    인터페이스

---
+++ 08_polymorphismR/src/edu/kh/poly/ex2/model/vo/HGD.java
@@ -0,0 +1,11 @@
+package edu.kh.poly.ex2.model.vo;
+
+public class HGD implements KH {
+
+	@Override
+	public void lesson() {
+		System.out.println("B강의장에서 ㅂ안 엔지니어 과정 수강");
+	}
+	
+
+}
---
+++ 08_polymorphismR/src/edu/kh/poly/ex2/model/vo/KH.java
@@ -0,0 +1,9 @@
+package edu.kh.poly.ex2.model.vo;
+
+public interface KH { // 인터페이스 
+
+	public static final String ADDRESS = "서울시 중구 남대문로 120";
+	
+	// 메서드(public abstract)
+	public abstract void lesson();
+}
